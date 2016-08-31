6dd811e30e3ea9fd27380bc9b72cec3d529e74fe
daniellasheva <daniellasheva@gmail.com>
2016-08-31 16:26:15 -0400

    initial commit

--- js/donut-counter.js
+++ js/donut-counter.js
@@ -7,3 +7,19 @@
 //write a conditional to check if there are enough donuts
 
   //alert the user with a message telling them the numbers of donuts and guests, and if there are enough donuts for each guest.
+
+
+
+var guests = prompt("Please insert number of guests");
+
+var donuts = prompt("Please insert number of donuts");
+
+  guests= parseInt(guests);
+  donuts= parseInt(donuts);
+
+  if (donuts > guests) {
+    alert(`There are ${guests} guests and ${donuts} donuts. You have enough donuts for each guest!`)
+  }
+  else {
+    alert(`There are ${guests} guests and ${donuts} donuts. You do not have enough donuts for each guest :(`)
+  }
